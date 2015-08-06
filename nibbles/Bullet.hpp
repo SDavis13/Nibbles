3fc51144a5b5bc2375ee06b1a8acb2aab3bb59e0
violentorchid <aduchanekmye13@winona.edu>
2015-08-06 14:58:41 -0500

    small changes to bullet
    compiles and runs!

--- nibbles/Bullet.hpp
+++ nibbles/Bullet.hpp
@@ -12,10 +12,10 @@ public:
 	b2Vec2 initialVelocity;
 	float angle;
 	Bullet(glm::vec3 position, glm::vec3 rotationAxis, float angle, b2Vec2 velocity ) //overriding scale
-            : Entity(position, rotationAxis, angle(*this.angle), glm::vec3 (0.1f, 0.1f, 0.1f), initialVelocity(velocity) ) {}
+            : Entity(position, rotationAxis, angle, glm::vec3 (0.1f, 0.1f, 0.1f)), initialVelocity(velocity) {}
     Bullet() : Entity() {}
     virtual void initialize(int type);
     virtual void behavior();
-}
+};
 
 #endif
