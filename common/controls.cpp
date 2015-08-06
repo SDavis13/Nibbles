0d30f5443f58bcf5b30586000f82b5f01f1141ca
Spencer Davis <SDavis13@winona.edu>
2015-08-06 10:19:48 -0500

    minor edits

--- common/controls.cpp
+++ common/controls.cpp
@@ -1,3 +1,5 @@
+#include <glew/glew.h>
+
 // Include GLFW
 #include <glfw3.h>
 extern GLFWwindow* window; // The "extern" keyword here is to access the variable "window" declared in tutorialXXX.cpp. This is a hack to keep the tutorials simple. Please avoid this.
--- common/controls.hpp
+++ common/controls.hpp
@@ -1,8 +1,6 @@
 #ifndef CONTROLS_HPP
 #define CONTROLS_HPP
 
-class globals;
-
 void computeMatricesFromInputs();
 glm::mat4 getViewMatrix();
 glm::mat4 getProjectionMatrix();
